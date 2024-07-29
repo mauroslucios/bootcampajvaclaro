44782cb549600965a1751640ca0d81c57cac5511
maurosluciosprojetos <maurosluciosprojetos@gmail.com>
2024-07-29 18:40:47 -0300

    diferença entre método e variável

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String primeiroNome = "Mauro";
+        String segundoNome = "Pereira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
