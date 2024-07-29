34f09d4362bbdab25362f4cbd495e00e33110384
maurosluciosprojetos <maurosluciosprojetos@gmail.com>
2024-07-29 19:57:40 -0300

    identação com método BoletimEstudantil

--- src/App.java
+++ src/App.java
@@ -1,13 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String primeiroNome = "Mauro";
-        String segundoNome = "Pereira";
+        
+       System.out.println(BoletimEstudantil.mediaFinal(0, null, null));
 
-        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
-        System.out.println(nomeCompleto);
     }
-
-    public static String nomeCompleto(String primeiroNome, String segundoNome){
-        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
-    }
+    
 }
